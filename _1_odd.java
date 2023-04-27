d0fcc70b32c1f714f0706e468ac6cb53f4573baf
Saniyaaahh <yasminsaniya135@gmail.com>
2023-04-28 00:03:01 +0530

    27th April

---
+++ _1_odd.java
@@ -0,0 +1,15 @@
+public class _1_Odd
+{
+    public static void main (String args [])
+    {
+        int a = 8;
+        if (a % 2 == 0)
+        {
+            System.out.println(a + " is Even");
+        }
+        else 
+        {
+            System.out.println(a + " is Odd");
+        }
+    }
+}
